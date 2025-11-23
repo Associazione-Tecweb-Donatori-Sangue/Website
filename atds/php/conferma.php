d9b34b9831ca5b153ec83ecf188799f985032ae5
Mario1535 <mariocurzio.depasquale@gmail.com>
2025-11-23 17:28:17 +0100

    Revert "Add files via upload"
    This reverts commit 61213c322abe049456e62510560b4d2052274c81.

--- atds/php/conferma.php
+++
@@ -1,8 +0,0 @@
-<?php
-$page_title = "Prenotazione Confermata";
-require_once "../php/header.php";
-?>
-<h1>Prenotazione avvenuta con successo!</h1>
-<p>Grazie <?php echo $_SESSION['username']; ?> per aver prenotato la tua donazione.</p>
-
-<?php require_once "../php/footer.php"; ?>
