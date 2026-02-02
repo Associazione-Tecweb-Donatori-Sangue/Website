9f9758118e80af1147ae7eee92b2561a65966f50
Diana Georgescu <daiangeo@gmail.com>
2026-02-02 14:35:12 +0100

    fixati commenti

--- src/php/pages/about_us.php
+++ src/php/pages/about_us.php
@@ -3,9 +3,7 @@ require_once "../utility.php";
 
 $paginaHTML = caricaTemplate('about_us.html');
 
-// 2. Definisco il breadcrumb per questa pagina
 $breadcrumb = '<p><a href="../../index.php" lang="en">Home</a> / <span>Chi siamo</span></p>';
 
-// 3. Costruisco e stampo la pagina finale
 echo costruisciPagina($paginaHTML, $breadcrumb, 'about_us.php');
 ?>
